29705d17480c4a6a434750d51e410a9547c877f9
d0lbangi <d0lbangi13@naver.com>
2023-03-07 02:52:44 +0900

    Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,19 +3,30 @@ import { useRouter } from "next/router";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+
   const router = useRouter();
+
   return (
-    <>
-      <nav>
-        <Link href="/" legacyBehavior>
-          {/* <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a> */}
-          <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
-        </Link>
-        <Link href="/about" legacyBehavior>
-          {/* <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a> */}
-          <a className={[styles.link,router.pathname === "/about" ? styles.active : "",].join(" ")}>About</a>
-        </Link>
-      </nav>
-    </>
+    <nav>
+      <Link href="/" legacyBehavior>
+        {/* <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a> */}
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        {/* <a className={[styles.link, router.pathname === "/about" ? styles.active : "",].join(" ")}>About</a> */}
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+      </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
+    </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,13 @@ export default function Home() {
   // const [counter, setCounter] = useState(0);
   return (
     <div>
-      {/* <h1>Hello {counter}</h1> */}
-      {/* <button onClick={() => setCounter((prev) => prev + 1)}>+</button> */}
-      <NavBar />
-      <h1>Hello</h1>
+      <NavBar/>
+      <h1 className="active">Hello</h1>
+      <style jsx>{`
+        a {
+          color: white;
+        }
+      `}</style>
     </div>
   );
 }
