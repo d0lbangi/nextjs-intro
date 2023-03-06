70a01e53e6053198f9dc5827acbfa58af44b7986
d0lbangi <d0lbangi13@naver.com>
2023-03-07 01:40:14 +0900

    Routing updates

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,14 +5,8 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
-          About
-        </a>
-      </Link>
+      <Link href="/" style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</Link>
+      <Link href="/about" style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</Link>
     </nav>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,6 @@
 import NavBar from "../components/NavBar";
 
 export default function Potato() {
-  return "about us";
   return (
     <div>
       <NavBar />
