dcdec4e29de500c054ced562572bb6b0c930fb5f
d0lbangi <d0lbangi13@naver.com>
2023-03-07 19:38:55 +0900

    Server Side Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,33 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
+export default function Home({results}) {
+  // const [movies, setMovies] = useState();
   // useEffect(()=>{}, []) 기본형
-  useEffect(()=>{
+  // useEffect(()=>{
     // (async()=>{})(); 기본형
-    (async()=>{
+    // (async()=>{
     //   const { results } = await (
     //     await fetch(
     //       `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
     //   )
     // ).json();
     // console.log(data);
-    const { results } = await (await fetch(`/api/movies`)).json();
-    setMovies(results);
-    })();
-  }, [])
+  //   const { results } = await (await fetch(`/api/movies`)).json();
+  //   setMovies(results);
+  //   })();
+  // }, [])
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie)=> (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {/* {movies?.map((movie)=> ( */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>
-    ))}
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
       <style jsx>{`
         .container {
           display: grid;
@@ -50,4 +51,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
