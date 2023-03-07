0777f8da4fcd6bb3f71d33e02c55b5c3e44cea04
d0lbangi <d0lbangi13@naver.com>
2023-03-07 20:48:45 +0900

    Recap

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({results}) {
       {/* {movies?.map((movie)=> ( */}
       {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -34,6 +34,9 @@ export default function Home({results}) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
